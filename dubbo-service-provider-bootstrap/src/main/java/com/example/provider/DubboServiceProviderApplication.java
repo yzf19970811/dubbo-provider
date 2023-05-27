6cb6cd3f34d9f40e49abe0f34c56cbafe52d911e
yuzhifa <893060089@qq.comm>
2023-05-27 13:59:41 +0800

    dubbo nacos

--- dubbo-service-provider-bootstrap/src/main/java/com/example/provider/DubboServiceProviderApplication.java
+++ dubbo-service-provider-bootstrap/src/main/java/com/example/provider/DubboServiceProviderApplication.java
@@ -1,17 +1,25 @@
 package com.example.provider;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author kyu.yzf
  * @date 2023/5/19 23:18
  */
 @SpringBootApplication
+@EnableDubbo
+@ImportResource(locations={"classpath*:spring/*.xml"})
 public class DubboServiceProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DubboServiceProviderApplication.class);
+        try {
+            SpringApplication.run(DubboServiceProviderApplication.class);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 }
--- dubbo-service-provider-service/src/main/java/com/example/provider/impl/DubboProvideServiceImpl.java
+++ dubbo-service-provider-service/src/main/java/com/example/provider/impl/DubboProvideServiceImpl.java
@@ -1,15 +1,31 @@
 package com.example.provider.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.example.provider.DubboProvideService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author kyu.yzf
  * @date 2023/5/19 23:22
  */
+@Service(version = "1.0.0")
 public class DubboProvideServiceImpl implements DubboProvideService {
 
     @Override
     public String sayHello() {
-        return "hello, call dubbo service success!!!";
+        RpcContext context = RpcContext.getContext();
+        String methodName = context.getMethodName();
+        String localHost = context.getLocalHost();
+        int localPort = context.getLocalPort();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("methodName", methodName);
+        map.put("localHost", localHost);
+        map.put("localPort", String.valueOf(localPort));
+
+        return map.toString();
     }
 }
